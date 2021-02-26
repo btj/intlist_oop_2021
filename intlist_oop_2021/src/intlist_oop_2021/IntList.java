b5603dfe1231f8bc82a28c55d24793cd6c735380
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2021-02-26 12:23:24 +0100

    Immutable IntList, implemented using a linked list

--- intlist_oop_2021/src/intlist_oop_2021/IntList.java
+++ intlist_oop_2021/src/intlist_oop_2021/IntList.java
@@ -11,19 +11,56 @@ import java.util.Arrays;
  */
 public class IntList {
 	
+	private static class Node {
+		private int value;
+		/**
+		 * @peerObject
+		 */
+		private Node next;
+		
+		private static boolean hasLength(Node node, int length) {
+			if (length == 0)
+				return node == null;
+			else
+				return length > 0 && node != null && hasLength(node.next, length - 1);
+		}
+	}
+	
 	/**
-	 * @invar | elements != null
-	 * 
+	 * @invar | Node.hasLength(head, length)
+	 */
+	private int length;
+	/**
 	 * @representationObject
 	 */
-	private int[] elements;
+	private Node head;
 
 	/**
 	 * Returns an array containing the elements of this object.
 	 * 
 	 * @creates | result
 	 */
-	public int[] getElements() { return elements.clone(); }
+	public int[] getElements() {
+		int[] result = new int[length];
+		Node node = head;
+		int i = 0;
+		while (node != null) {
+			result[i++] = node.value;
+			node = node.next;
+		}
+		return result;
+	}
+	
+	private static Node createNodes(int[] elements, int index) {
+		if (index == elements.length)
+			return null;
+		else {
+			Node head = new Node();
+			head.value = elements[index];
+			head.next = createNodes(elements, index + 1);
+			return head;
+		}
+	}
 	
 	/**
 	 * Initializes this object so that it represents the sequence of int values contained in the given array.
@@ -33,7 +70,8 @@ public class IntList {
 	 * @post | Arrays.equals(getElements(), elements)
 	 */
 	public IntList(int[] elements) {
-		this.elements = elements.clone();
+		this.head = createNodes(elements, 0);
+		this.length = elements.length;
 	}
 
 }
